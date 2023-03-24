93c5c3e9440eed01745f461c7999f594a261c2de
Gingerus <117784746+Gingerus@users.noreply.github.com>
2023-03-23 21:44:11 -0300

    Assignment 01 - Intro to Java & OOP

---
+++ Assignment 01 - Intro to Java & OOP/MyArea/src/MyArea.java
@@ -0,0 +1,20 @@
+/*
+Filename: MyArea.java
+Author: Nathan L'Abbé
+Date: 20th of March 2023
+Purpose: To determine the area of a surface given the length and width in meters.
+ */
+
+public class MyArea
+{
+    public static void main(String[] args)
+    {
+        double length = 22;
+        double width = 8.5;
+        double area = length * width;
+
+        System.out.println("Given the Length "+length+"m and Width of "+width+"m.");
+        System.out.println("The area for the rectangle is: "+area"m");
+
+    }
+}
---
+++ Assignment 01 - Intro to Java & OOP/Paycheck/src/Paycheck.java
@@ -0,0 +1,21 @@
+/*
+Filename: SmallestInt.java
+Author: Nathan L'Abbé
+Date: 20th of March 2023
+Purpose: Given the user's hourly rate of pay, and the number of hours worked. Display the Following
+Display the user's gross pay, the withholding tax (10% of gross pay), and the net pay (gross pay - withholding).
+ */
+
+public class Paycheck {
+    public static void main(String[] args)
+    {
+        double hourlyPay;
+        double hoursWorked;
+        
+        if (args.length == 0)
+        {
+
+
+        }
+    }
+}
